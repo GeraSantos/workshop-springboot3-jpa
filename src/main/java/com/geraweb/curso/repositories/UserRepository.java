f97d8dafa98856730579e3fb18aa8d264d5a820c
Geraldo Santos <geraldo.gsantos12@gmail.com>
2023-01-14 23:12:38 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/geraweb/curso/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.geraweb.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.geraweb.curso.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+}
