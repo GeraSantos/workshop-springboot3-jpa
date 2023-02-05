b0c074a8f25109485d51122872231003c8cbbb75
Geraldo Santos <geraldo.gsantos12@gmail.com>
2023-02-05 14:00:19 -0300

    Procurando divergências no codigo, indicios de erros

--- src/main/java/com/geraweb/curso/entities/Order.java
+++ src/main/java/com/geraweb/curso/entities/Order.java
@@ -2,7 +2,6 @@ package com.geraweb.curso.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
-import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -63,7 +62,10 @@ public class Order implements Serializable{
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -75,7 +77,12 @@ public class Order implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		Order other = (Order) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+				if (other.id != null)
+					return false;
+		} else if (!id.equals(other.id))
+				return false;
+		return true;
 	}
 	
 	
--- src/main/java/com/geraweb/curso/entities/User.java
+++ src/main/java/com/geraweb/curso/entities/User.java
@@ -3,7 +3,6 @@ package com.geraweb.curso.entities;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -89,7 +88,10 @@ public class User implements Serializable {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -101,7 +103,12 @@ public class User implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		User other = (User) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 
 	
